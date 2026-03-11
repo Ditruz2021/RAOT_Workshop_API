56748a5fa43faef97605c96b71a3d3170b9cb351
Nont Sanarmart <nontiwat@ditruz.com>
2026-03-11 09:19:09 +0700

    Chore: เพิ่ม Dto RoleResponse

--- Dto/UserModel.cs
+++ Dto/UserModel.cs
@@ -42,4 +42,10 @@
         public int RoleId { get; set; }
         public string RoleName { get; set; } = null!;
     }
+
+    public class RoleResponse
+    {
+        public int RoleId { get; set; }
+        public string RoleName { get; set; } = string.Empty;
+    }
 }
